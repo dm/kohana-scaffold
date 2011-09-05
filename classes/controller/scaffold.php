cb6aaa8b735d5f8f5f698f09ce8ec98fad5b5e93
Daniel Macedo <admacedo@gmail.com>
2011-09-05 11:45:39 +0100

    Compatible with Kohana v3.1.x

--- classes/controller/scaffold.php
+++ classes/controller/scaffold.php
@@ -3,15 +3,15 @@
 Class Controller_Scaffold extends Controller {
 
 	protected $column = '';
-	
+
 	protected $auto_modeler = TRUE;
-	
+
 	protected $items_per_page = 15;
-	
+
 	protected $db_first = "";
-	
+
 	protected $header = Array();
-	
+
 	protected function _get_schema( $force = FALSE ) {
 		$column = ImplodeUppercase::decode( $this->column );
 		if ( empty( $this->header ) || $force )
@@ -26,16 +26,16 @@ Class Controller_Scaffold extends Controller {
 			};
 		}
 	}
-	
+
 	public function before()
 	{
 	}
-	
+
 	public function flash( $msg, $type = "success" )
 	{
 		$last = Session::instance()->get("flash.message");
 		$new = array(
-			"msg" => $msg, 
+			"msg" => $msg,
 			"type" => $type
 		);
 		if ( $last !== NULL )
@@ -46,7 +46,7 @@ Class Controller_Scaffold extends Controller {
 		};
 		Session::instance()->set("flash.message",  $new);
 	}
-	
+
 	protected function _auto_model( $model = NULL )
 	{
 		$success = FALSE;
@@ -84,7 +84,7 @@ Class Controller_Scaffold extends Controller {
 				$db = Database::instance()->list_columns( $this->column );
 				$_primary_key = "";
 				$_primary_val = "";
-				
+
 				foreach ( $db as $collum ) {
 					if ( ( $_primary_key !== "" ) && ( $_primary_val === "" ) && $collum["type"] === "string" ) {
 						$_primary_val = $collum["column_name"];
@@ -100,13 +100,13 @@ class Model_Scaffold_". $class_name ." extends ORM
     protected \$_table_name  = '". str_replace("scaffold_", "", $this->column ) ."';
     protected \$_primary_key = '$_primary_key';
     protected \$_primary_val = '$_primary_val';
- 
+
     protected \$_table_columns = array(\n";
 				foreach ( $db as $collum ) {
 					$model_container .= "\t\t'". $collum["column_name"] ."' => array('data_type' => '". $collum["type"] ."', 'is_nullable' => ". ( ( $collum["is_nullable"] ) ? "TRUE" : "FALSE" ) ."),\n";
 				};
 				$model_container .= "\t);\n}";
-				
+
 				if ( ! is_dir($path) )
 				{
 					mkdir($path, 0777, TRUE);
@@ -145,16 +145,16 @@ class Model_Scaffold_". $class_name ." extends ORM
 				unlink( $fname );
 			};
 		};
-		
+
 		if ( $i > 0 )
 		{
 			$this->flash("$i new models");
 		} else {
 			$this->flash("No new model found", "notice");
 		};
-		Request::instance()->redirect("scaffold");
+		$this->request->redirect("scaffold");
 	}
-	
+
 	protected function remove_models()
 	{
 		$path = APPPATH.'classes'.DIRECTORY_SEPARATOR ."model" . DIRECTORY_SEPARATOR . "scaffold" . DIRECTORY_SEPARATOR;
@@ -169,13 +169,13 @@ class Model_Scaffold_". $class_name ." extends ORM
 			$count = "No";
 		};
 		$this->flash("$count models removed", "notice");
-		Request::instance()->redirect("scaffold");
+		$this->request->redirect("scaffold");
 	}
 
 	public function action_index()
 	{
 		$content = Array();
-		
+
 		if ( isset($_GET["auto_modeler"]) )
 		{
 			if ( empty( $_GET["auto_modeler"] ) )
@@ -185,7 +185,7 @@ class Model_Scaffold_". $class_name ." extends ORM
 				$this->auto_modeler( $_GET["auto_modeler"] );
 			};
 		};
-		
+
 		if ( isset($_GET["remove_models"]) )
 		{
 			if ( empty( $_GET["remove_models"] ) )
@@ -195,15 +195,15 @@ class Model_Scaffold_". $class_name ." extends ORM
 				$this->remove_models( $_GET["remove_models"] );
 			};
 		};
-		
+
 		$subPath =  ( isset($_GET["dir"]) ) ? $_GET["dir"] : "";
 		$path = APPPATH.'classes' . DIRECTORY_SEPARATOR . "model" . DIRECTORY_SEPARATOR  . "scaffold" . DIRECTORY_SEPARATOR . $subPath;
-		
+
 		if ( ! is_dir($path) )
 		{
 			mkdir($path, 0777, TRUE);
 		};
-		
+
 		if ($handle = opendir($path)) {
 			$files = Array();
 			$directores = Array();
@@ -216,26 +216,26 @@ class Model_Scaffold_". $class_name ." extends ORM
 				};
 			};
 			closedir($handle);
-			
+
 			foreach ( $directores as $item )
 			{
 				$item_name = str_replace(Array($path, EXT), "", $item);
 				// array_push( $content, HTML::anchor('scaffold?dir='.$item_name, "[+] " . ucfirst($item_name)) );
 				// array_push( $content, "[+] " . ucfirst($item_name) );
 			};
-			
+
 			foreach ( $files as $item )
 			{
 				$item_name = str_replace(Array($path, EXT), "", $item);
 				array_push( $content, HTML::anchor('scaffold/list/'.$subPath.$item_name, ImplodeUppercase::ucwords_text($item_name)) );
 			};
 		};
-		
+
 		if ( empty($content) )
 		{
 			$content = __("No models to list");
 		};
-		
+
 		$data = Array(
 			"content" => $content,
 			"msg" => ( isset($_GET["msg"]) ? $_GET["msg"] : "" ),
@@ -243,32 +243,32 @@ class Model_Scaffold_". $class_name ." extends ORM
 		);
 		echo View::factory("scaffold/index", $data)->render();
 	}
-	
+
 	public function action_list( $request = NULL )
 	{
 		if ( empty( $request ) )
 		{
-			Request::instance()->redirect('scaffold');
+			$this->request->redirect('scaffold');
 		};
 		$this->column = $request;
 		$this->_get_schema();
-		
+
 		if ( $this->column === "" ) {
 			echo "<p>". __("Please, select a column") . "</p>";
 			exit;
 		};
-		
+
 		$orm = ORM::factory("scaffold_".$this->column);
-		
-		$controller = url::base() . request::instance()->controller;
-		
+
+		$controller = url::base() . Request::current()->controller();
+
 		$this->items_per_page = ( isset( $_GET["items_per_page"] ) ) ? $_GET["items_per_page"] : $this->items_per_page;
-		
+
 		$pagination = Pagination::factory(array(
 			'total_items'    => $orm->count_all(),
 			'items_per_page' => $this->items_per_page
 		));
-		
+
 		$query = $orm
 			->limit( $pagination->items_per_page )
 			->offset( $pagination->offset )
@@ -281,12 +281,12 @@ class Model_Scaffold_". $class_name ." extends ORM
 			foreach ( $key as $value ) {
 				array_push($item, $value);
 			};
-			
+
 			$id = $key[$this->db_first];
-			array_push($item, "<a href=\"$controller/edit/". $this->column ."/$id\">". __("Edit") ."</a> | <a href=\"$controller/delete/". $this->column ."/$id\"  class=\"delete\">". __("Delete") ."</a>");	
+			array_push($item, "<a href=\"$controller/edit/". $this->column ."/$id\">". __("Edit") ."</a> | <a href=\"$controller/delete/". $this->column ."/$id\"  class=\"delete\">". __("Delete") ."</a>");
 			array_push($result, $item);
 		};
-		
+
 		$data = Array(
 			"column" => ucfirst(str_replace("_"," ",$this->column)),
 			"db_first" => $this->db_first,
@@ -296,18 +296,18 @@ class Model_Scaffold_". $class_name ." extends ORM
 			"msg" => ( isset($_GET["msg"]) ? $_GET["msg"] : NULL ),
 			"msgtype" => ( isset($_GET["msgtype"]) ? $_GET["msgtype"] : "success" )
 		);
-		
+
 		echo View::factory("scaffold/list", $data)->render();
 	}
-	
+
 	public function action_insert( $request )
 	{
 		if ( $request === "save" ) {
 			$this->column = $_POST["column"];
 			unset( $_POST["column"] );
-			
+
 			$orm = ORM::factory("scaffold_".$this->column)->values( $_POST );
-			
+
 			if ( $orm->check() ) {
 				$orm->save();
 				$this->flash(__('Record updated successfully!'));
@@ -315,7 +315,7 @@ class Model_Scaffold_". $class_name ." extends ORM
 				$errors = $orm->validate()->errors();
 				$this->flash($errors, "error");
 			};
-			Request::instance()->redirect("scaffold/list/". $this->column . "/");
+			$this->request->redirect("scaffold/list/". $this->column . "/");
 		} else {
 			$this->column = $request;
 			$this->_get_schema();
@@ -327,12 +327,12 @@ class Model_Scaffold_". $class_name ." extends ORM
 			echo View::factory("scaffold/insert", $data)->render();
 		};
 	}
-	
+
 	public function action_edit( $request, $id )
 	{
 		$this->column = $request;
 		$this->_get_schema(TRUE);
-		
+
 		$orm = ORM::factory("scaffold_".$this->column, $id)->as_array();
 
 		$data = Array(
@@ -341,36 +341,36 @@ class Model_Scaffold_". $class_name ." extends ORM
 			"first" => $this->db_first,
 			"content" => $orm
 		);
-		
+
 		echo View::factory("scaffold/edit", $data)->render();
 	}
-	
+
 	public function action_save()
 	{
 		$id = array_keys($_POST);
 		$this->column = $_POST["column"];
 		unset( $_POST["column"] );
-		
+
 		$orm = ORM::factory("scaffold_".$this->column, array_shift( $_POST ))->values( $_POST );
-		
+
 		if ($orm->check()) {
 			$orm->save();
-			Request::instance()->redirect('scaffold/list/'. $this->column .'/?msg='. __('Record updated successfully') .'!');
+			$this->request->redirect('scaffold/list/'. $this->column .'/?msg='. __('Record updated successfully') .'!');
 		} else {
 			$errors = $orm->validate()->errors();
-			Request::instance()->redirect("scaffold/list/". $this->column . "/?msg=$errors&msgtype=error");
+			$this->request->redirect("scaffold/list/". $this->column . "/?msg=$errors&msgtype=error");
 		}
 	}
-	
+
 	public function action_delete($request, $id)
 	{
 		$this->column = $request;
 		$this->_get_schema();
-		
+
 		$orm = ORM::factory("scaffold_".$this->column, $id)->delete();
 
 		$this->flash( __("Registration $request successfully deleted"), "error" );
-		Request::instance()->redirect("scaffold/list/". $request);
+		$this->request->redirect("scaffold/list/". $request);
 	}
 
 }
